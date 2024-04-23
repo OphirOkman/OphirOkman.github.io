f3a6e96016be5d8e6acac40269ec65ffb5724889
Ophir Okman <ophirok88@gmail.com>
2024-04-23 23:07:31 +0300

    New files added to repository comments.js and index.html

---
+++ comments.js
@@ -0,0 +1,10 @@
+// this is an example of using git in the work flow:
+
+// 1. I write one part of the code
+// 2. another developer add his part
+// 3. we merge our work together
+
+//Problems that git solves:
+
+// 1. You can always go back to the code before the latest changes were made
+// 2. Each software engineer work in his local environment without the need of a server
