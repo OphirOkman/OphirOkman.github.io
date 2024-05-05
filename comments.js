97c5cde4c8a13b1c06fd00cfbec09650444f9a9b
Ophir Okman <ophirok88@gmail.com>
2024-05-06 00:32:16 +0300

    changed file

--- comments.js
+++ comments.js
@@ -1,14 +1,14 @@
 // Function to navigate between sections
 function navigateTo(sectionName) {
   var content = document.getElementById("content");
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
+  var http = new XMLHttpRequest();
+  http.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       content.innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", sectionName + ".html", true);
-  xhttp.send();
+  http.open("GET", sectionName + ".html", true);
+  http.send();
 }
 
 // Load default content on page load
